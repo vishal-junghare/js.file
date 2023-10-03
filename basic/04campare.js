3a10dccbe5602fc961d3e0d17287053594d84db4
Vishal Junghare <141499707+vishal-junghare@users.noreply.github.com>
2023-10-03 07:45:58 +0000

    camparise opration

---
+++ basic/04campare.js
@@ -0,0 +1,10 @@
+// console.log(null > 0);
+// // === strick check 
+// console.log("2" === 2);
+// console.log("2" == 2);
+// // 
+console.log(null > 0);//false 
+console.log(null == 0);// false
+console.log(null >= 0);//true
+console.log(null <= 0); // true
+//******************** nxt opreation ********************
