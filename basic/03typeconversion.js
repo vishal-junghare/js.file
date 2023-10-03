a3bb8bc49afc5497ee4d41b0a8c9e831154b4354
Vishal Junghare <141499707+vishal-junghare@users.noreply.github.com>
2023-10-03 06:14:49 +0000

    type conversion datatype

---
+++ basic/03typeconversion.js
@@ -0,0 +1,18 @@
+let score=undefined;
+//console.log(score);
+//console.log(typeof(score))
+let valueInNumber = Number(score);
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber)
+ // "33" => 33;
+ // "33avc" => NaN;
+ // true=>1 false => 0 
+ // "" => false [] => true 
+ // "vishal" => true null => false
+ let isBoolean =""
+ let booleanIsLoggedIn=Boolean(isBoolean);
+ //console.log(booleanIsLoggedIn)
+ let someNumber=33;
+ let stringNmuber=String(someNumber);
+ console.log(typeof(stringNmuber));
+ console.log(stringNmuber)
