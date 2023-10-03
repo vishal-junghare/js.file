540aa812a5f2d585c1547160d5fcc067d09d8d45
Vishal Junghare <141499707+vishal-junghare@users.noreply.github.com>
2023-10-03 05:44:55 +0000

    v

---
+++ basic/01variable.js
@@ -0,0 +1,9 @@
+const accountId = 06021994; // this is the constant value not change
+let accountEmail = "vishaljunghare0430@gmail.com"; // this {let} variable always change it value
+
+var accountPassword = "Vishal6294@"; // this {let} variable always change it value 
+accountCity = "Chandrapur"; // possiblely of not varible mention so  ouput
+ accountState;
+
+/*  pefer to not use for  beacuse in issue in block scoop  */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) //this conole table methods are output show ths table form
---
+++ basic/02datatype.js
@@ -0,0 +1,23 @@
+"user strict" ;// treat all js code as never version
+//alert("3+3") // we are use node js not browers//
+console.log("vishal"); // code readability should 
+
+let number = 18;
+let name ="vishal";
+let isloggdIn=false;
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+//boolean => true or false
+// null => standalone value
+//undefined
+// symbol=> it use react it use uique
+
+
+console.log(typeof("vishal"));
+console.log(typeof(45));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
